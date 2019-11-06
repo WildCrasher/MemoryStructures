a91b8cf24fd64d045f55ed188f0f0e23d0db058e
in127278 <maciej.ossig@student.put.poznan.pl>
2019-11-06 11:48:41 +0100

    Add interval search

--- main.py
+++ main.py
@@ -54,22 +54,8 @@ class ArrayStructure:
     def find_element(self, value: int):
         return list(filter(lambda x: x.value is value, self.data))
 
-    def find_elements(self, valueStart: int, valueEnd: int):
-        return [element for element in self.data if valueStart <= element.value <= valueEnd]
-
-
-class AVLWrapper:
-    def __init__(self):
-        self.root = AVLTree()
-
-    def insert_element(self, element: Data):
-        self.root.insert(element.value, element.text)
-
-    def remove_element(self, value: int):
-        self.root.remove(value)
-
-    def find_element(self, value: int):
-        return self.root.get_value(value)
+    def find_elements(self, start: int, end: int):
+        return [element for element in self.data if start <= element.value <= end]
 
 
 class BinaryWrapper:
@@ -85,6 +71,21 @@ class BinaryWrapper:
     def find_element(self, value: int):
         return self.root.get_value(value)
 
+    def find_elements(self, start, end):
+        return self.root.item_slice(start, end)
+
+
+class AVLWrapper(BinaryWrapper):
+    def __init__(self):
+        BinaryWrapper.__init__(self)
+        self.root = AVLTree()
+
+
+class RBWrapper(BinaryWrapper):
+    def __init__(self):
+        BinaryWrapper.__init__(self)
+        self.root = RBTree()
+
 
 class TimeMeasure:
     def __init__(self):
@@ -92,6 +93,7 @@ class TimeMeasure:
         self.insert = 0
         self.delete = 0
         self.find = 0
+        self.f_interval = 0
 
 
 def insert_elements(structure, elements):
@@ -118,6 +120,13 @@ def find_elements(structure, elements):
     return end - start
 
 
+def find_elements_interval(structure, f_min, f_max):
+    start = timer()
+    structure.find_elements(f_min, f_max)
+    end = timer()
+    return end - start
+
+
 def get_random_elements(dataset, sample_size):
     return sample(dataset, sample_size)
 
@@ -140,10 +149,13 @@ if __name__ == '__main__':
     construct_interval = 100  # timestamps frequency while constructing
     to_insert = 100  # number of elements to insert
     to_delete = 100  # number of elements to delete
+    find_min = 0  # TODO automatically generate period when given %
+    find_max = 9000000
 
     array_time = TimeMeasure()
     avl_time = TimeMeasure()
     binary_time = TimeMeasure()
+    rb_time = TimeMeasure()
 
     data_set = DataSet(size)
     insert_set = DataSet(to_insert).randomSet
@@ -154,26 +166,47 @@ if __name__ == '__main__':
     array_time.insert = insert_elements(array_structure, insert_set)
     array_time.delete = delete_elements(array_structure, delete_set)
     array_time.find = find_elements(array_structure, insert_set)
+    array_time.f_interval = find_elements_interval(array_structure, find_min, find_max)
 
     avl = AVLWrapper()
     avl_time.construct = construct(avl, data_set.randomSet, construct_interval)
     avl_time.insert = insert_elements(avl, insert_set)
     avl_time.delete = delete_elements(avl, delete_set)
     avl_time.find = find_elements(avl, insert_set)
+    avl_time.f_interval = find_elements_interval(avl, find_min, find_max)
 
     binary = BinaryWrapper()
     binary_time.construct = construct(binary, data_set.randomSet, construct_interval)
     binary_time.insert = insert_elements(binary, insert_set)
     binary_time.delete = delete_elements(binary, delete_set)
     binary_time.find = find_elements(binary, insert_set)
+    binary_time.f_interval = find_elements_interval(binary, find_min, find_max)
+
+    rbtree = RBWrapper()
+    rb_time.construct = construct(rbtree, data_set.randomSet, construct_interval)
+    rb_time.insert = insert_elements(rbtree, insert_set)
+    rb_time.delete = delete_elements(rbtree, delete_set)
+    rb_time.find = find_elements(rbtree, insert_set)
+    rb_time.f_interval = find_elements_interval(rbtree, find_min, find_max)
 
     for i in range(len(avl_time.construct)):
-        print(f'{(i+1)*construct_interval} Array:{array_time.construct[i]} AVL:{avl_time.construct[i]} '
-              f'Binary:{binary_time.construct[i]}')
+        print(f'{(i+1)*construct_interval} '
+              f'Array:{array_time.construct[i]} '
+              f'AVL:{avl_time.construct[i]} '
+              f'Binary:{binary_time.construct[i]}' 
+              f'RB:{rb_time.construct[i]}'
+              )
     print(f'/// Insertion: ///\n'
-          f'Array: {array_time.insert} AVL: {avl_time.insert} Binary: {binary_time.insert} \n'
+          f'Array: {array_time.insert} AVL: {avl_time.insert} Binary: {binary_time.insert}  RB: {rb_time.insert}\n'
           f'/// Deletion: ///\n'
-          f'Array: {array_time.delete} AVL: {avl_time.delete} Binary: {binary_time.delete} \n'
+          f'Array: {array_time.delete} AVL: {avl_time.delete} Binary: {binary_time.delete} RB: {rb_time.delete}\n'
           f'/// Find elements: ///\n'
-          f'Array: {array_time.find} AVL: {avl_time.find} Binary: {binary_time.find} \n'
+          f'Array: {array_time.find} AVL: {avl_time.find} Binary: {binary_time.find} RB: {rb_time.find}\n'
+          f'/// Find elements (interval): ///\n'
+          f'Array: {array_time.f_interval} AVL: {avl_time.f_interval} Binary: {binary_time.f_interval} RB: {rb_time.f_interval}\n'
           )
+
+    # print(f'Array: {len(array_structure.find_elements(find_min, find_max))} '
+    #       f'AVL: {len(list(avl.find_elements(find_min, find_max)))} '
+    #       f'Binary: {len(list(binary.find_elements(find_min, find_max)))} '
+    #       f'RB: {len(list(rbtree.find_elements(find_min, find_max)))}')
